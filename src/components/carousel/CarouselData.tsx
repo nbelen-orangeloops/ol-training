e278c576c557a3590a7dddaf9e5d72a23feaa7d0
nbelen-orangeloops <nbelen@orangeloops.com>
2023-01-13 14:20:59 -0300

    Change dot pagination to instagram style

--- src/components/carousel/CarouselData.tsx
+++ src/components/carousel/CarouselData.tsx
@@ -1,13 +1,37 @@
 import React, { useState, useRef } from "react";
 import { View, Dimensions, Image } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Carousel, { Pagination } from "react-native-snap-carousel";
+import PaginationDot from "react-native-insta-pagination-dots";
+import Carousel from "react-native-snap-carousel";
 import WebView from "react-native-webview";
 
 export const SLIDER_WIDTH = Dimensions.get("window").width + 30;
 export const ITEM_WIDTH = Math.round(SLIDER_WIDTH);
 
 const images = [
+  {
+    category: "img",
+    url: "https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2thdGVib2FyZGluZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    category: "img",
+    url: "https://images.pexels.com/photos/58729/pexels-photo-58729.jpeg?cs=srgb&dl=pexels-salvio-bhering-58729.jpg&fm=jpg",
+  },
+  {
+    category: "img",
+    url: "https://images.ctfassets.net/3s5io6mnxfqz/5ZHWhb7IZYugQE6g9AiAb3/82664bc995fc0694d2ab2734d841c695/AdobeStock_266317440.jpeg",
+  },
+  {
+    category: "img",
+    url: "https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2thdGVib2FyZGluZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
+  },
+  {
+    category: "img",
+    url: "https://images.pexels.com/photos/58729/pexels-photo-58729.jpeg?cs=srgb&dl=pexels-salvio-bhering-58729.jpg&fm=jpg",
+  },
+  {
+    category: "img",
+    url: "https://images.ctfassets.net/3s5io6mnxfqz/5ZHWhb7IZYugQE6g9AiAb3/82664bc995fc0694d2ab2734d841c695/AdobeStock_266317440.jpeg",
+  },
   {
     category: "img",
     url: "https://images.unsplash.com/photo-1520045892732-304bc3ac5d8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2thdGVib2FyZGluZ3xlbnwwfHwwfHw%3D&w=1000&q=80",
@@ -65,8 +89,6 @@ const renderItem = ({ item }: any) => {
 
 export const CarouselComp = () => {
   const [index, setIndex] = useState(0);
-  const [autoPlay, setAutoPlay] = useState(true);
-  // const [stopAutoPlay, setStopAutoPlay] = useState(false);
   const isCarousel = useRef(null);
 
   const data = images.map((item, idx: number) => {
@@ -74,32 +96,33 @@ export const CarouselComp = () => {
   });
 
   return (
-    <View style={{ width: "100%" }}>
+    <View
+      style={{
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
       <Carousel
         ref={isCarousel}
         data={data}
         renderItem={renderItem}
-        layout="default"
+        layout="stack"
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         onSnapToItem={(index: number) => setIndex(index)}
         loop={true}
-        autoplay={autoPlay}
+        autoplay={true}
         autoplayInterval={3000}
         lockScrollWhileSnapping={true}
       />
-      <Pagination
-        dotsLength={data.length}
-        activeDotIndex={index}
-        carouselRef={isCarousel}
-        dotStyle={{
-          width: 10,
-          height: 10,
-          borderRadius: 10,
-          marginHorizontal: 8,
-          backgroundColor: "#FC8C27",
-        }}
-      />
+      <View style={{ padding: 20 }}>
+        <PaginationDot
+          activeDotColor={"#FC8C27"}
+          curPage={index}
+          maxPage={data.length}
+          sizeRatio={1.2}
+        />
+      </View>
     </View>
   );
 };
